2f4f1674d9d24ead946d08c4702cffd9894732ae
Harsh Narwariya <h.narwariya@thesynapses.com>
2024-08-06 17:14:43 +0530

    registration

--- src/formik/authooks.js
+++ src/formik/authooks.js
@@ -10,18 +10,18 @@ export const useRegisterFormik = ({ ...props }) =>
     },
     validationSchema: Yup.object().shape({
       username: Yup.string()
-        .min(3, "Username must be at least 3 characters")
-        .max(20, "Username must not exceed 20 characters")
+        // .min(3, "Username must be at least 3 characters")
+        // .max(20, "Username must not exceed 20 characters")
         .required("Username is required"),
       email: Yup.string()
-        .email("Invalid email address")
+        // .email("Invalid email address")
         .required("Email is required"),
       password: Yup.string()
-        .min(8, "Password must be at least 8 characters")
-        .matches(
-          /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
-          "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character"
-        )
+        // .min(8, "Password must be at least 8 characters")
+        // .matches(
+        //   /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+        //   "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character"
+        // )
         .required("Password is required"),
     }),
     ...props,
