b266a955db7c0fad788ff60fc1909363122462eb
Harsh Narwariya <h.narwariya@thesynapses.com>
2024-08-09 17:37:03 +0530

    haa bhai shayad logic galat likha hu main

--- src/pages/home/Tab.js
+++ src/pages/home/Tab.js
@@ -1,7 +1,22 @@
+import useTabState from "../../store/tabstate";
 import DirectTab from "./DirectTab";
 
+const HandleChatTab = () => {
+  const secondTab0 = useTabState((state) => state.secondTab0);
+  if (secondTab0 === 0) return <DirectTab />;
+  return null;
+};
+
+const HandleCallTab = () => {
+  // const secondTab1 = useTabState((state) => state.secondTab1);
+  return null;
+};
+
 const Tab = () => {
-  return <DirectTab />;
+  const firstTab = useTabState((state) => state.firstTab);
+  if (firstTab === 0) return <HandleChatTab />;
+  else if (firstTab === 1) return <HandleCallTab />;
+  else return null;
 };
 
 export default Tab;
