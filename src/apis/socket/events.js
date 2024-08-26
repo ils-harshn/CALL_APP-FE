11ac723fca85692293a5c429f6ffc73c2c536b59
Harsh Narwariya <h.narwariya@thesynapses.com>
2024-08-26 16:47:07 +0530

    maintain sockets

---
+++ src/apis/socket/events.js
@@ -0,0 +1,5 @@
+const SOCKET_EVENTS = {
+  CONNECTED: "CONNECTED",
+};
+
+export default SOCKET_EVENTS;
--- src/pages/layouts/protectors.js
+++ src/pages/layouts/protectors.js
@@ -2,11 +2,39 @@ import { Navigate } from "react-router-dom";
 import { useProfile } from "../../apis/auth/queryHooks";
 import { FullScreenSpinner } from "../../components/Loaders/FullScreenLoaders";
 import ROUTES from "../../routes";
+import { useEffect, useState } from "react";
+import { io } from "socket.io-client";
+import SOCKET_EVENTS from "../../apis/socket/events";
+
+const SOCKET_CONNECTING_STATES = {
+  CONNECTING: "CONNECTING",
+  CONNECTED: "CONNECTED",
+  FAILED: "FAILED",
+};
 
 export const CheckLogin = ({ children }) => {
   const { isLoading, isError } = useProfile();
+  const [socketState, setSocketState] = useState(
+    SOCKET_CONNECTING_STATES.CONNECTING
+  );
 
-  if (isLoading) return <FullScreenSpinner />;
-  if (isError) return <Navigate to={ROUTES.LOGIN} />;
+  useEffect(() => {
+    const socket = io(process.env.REACT_APP_API_URI);
+
+    const handleConnect = () => {
+      setSocketState(SOCKET_CONNECTING_STATES.CONNECTED);
+    };
+    
+    socket.on(SOCKET_EVENTS.CONNECTED, handleConnect);
+
+    return () => {
+      socket.off(SOCKET_EVENTS.CONNECTED, handleConnect);
+    };
+  }, []);
+
+  if (isLoading || socketState === SOCKET_CONNECTING_STATES.CONNECTING)
+    return <FullScreenSpinner />;
+  if (isError || socketState === SOCKET_CONNECTING_STATES.FAILED)
+    return <Navigate to={ROUTES.LOGIN} />;
   return children;
 };
