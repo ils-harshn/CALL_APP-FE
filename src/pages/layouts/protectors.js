46b43593ae4e147b9a0c7dfe77a8d87615565910
Harsh Narwariya <h.narwariya@thesynapses.com>
2024-08-26 15:41:16 +0530

    get back to login of not looged

--- src/pages/layouts/protectors.js
+++ src/pages/layouts/protectors.js
@@ -1,10 +1,12 @@
+import { Navigate } from "react-router-dom";
 import { useProfile } from "../../apis/auth/queryHooks";
 import { FullScreenSpinner } from "../../components/Loaders/FullScreenLoaders";
+import ROUTES from "../../routes";
 
 export const CheckLogin = ({ children }) => {
   const { isLoading, isError } = useProfile();
 
   if (isLoading) return <FullScreenSpinner />;
-  if (isError) return <h2>Login reqruired</h2>;
+  if (isError) return <Navigate to={ROUTES.LOGIN} />;
   return children;
 };
