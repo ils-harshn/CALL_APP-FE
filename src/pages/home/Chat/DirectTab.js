263b6514346cdd7d430564c3fb6621bc4d8c842b
Harsh Narwariya <h.narwariya@thesynapses.com>
2024-08-27 15:17:25 +0530

    last_message added

--- src/pages/home/Chat/DirectTab.js
+++ src/pages/home/Chat/DirectTab.js
@@ -3,12 +3,13 @@ import { useGetAcceptedConnections } from "../../../apis/connections/queryHooks"
 import useTabState from "../../../store/tabstate";
 import Skeleton from "react-loading-skeleton";
 import { InfiniteLoader, List as VList, AutoSizer } from "react-virtualized";
+import { DTMformatDate } from "../../../utils/dateFormaters";
 
 export const message_status = {
-  SEEN: 0,
-  SENT: 1,
-  SENDING: 2,
-  FAILED: 3,
+  SEEN: "seen",
+  SENT: "sent",
+  SENDING: "sending",
+  FAILED: "failed",
 };
 
 export const message_status_map = {
@@ -42,97 +43,6 @@ export const status_color = {
   [status.DND]: "bg-yellow-500",
 };
 
-// export const dtdata = [
-//   {
-//     id: 1,
-//     name: "Anushka Rawat",
-//     last_message: {
-//       message:
-//         "Hi, Anushka can we have a talk for a while, I need to tell you one last 3 words",
-//       status: message_status.SEEN,
-//       date: "09/08/24",
-//     },
-//     status: status.ONLINE,
-//   },
-//   {
-//     id: 2,
-//     name: "Harsh Narwariya",
-//     last_message: {
-//       message:
-//         "Hi, Anushka can we have a talk for a while, I need to tell you one last 3 words",
-//       status: message_status.SENT,
-//       date: "09/08/24",
-//     },
-//     status: status.OFFLINE,
-//   },
-//   {
-//     id: 3,
-//     name: "Jyoti Mishra",
-//     last_message: {
-//       message:
-//         "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
-//       status: message_status.SENDING,
-//       date: "09/08/24",
-//     },
-//     status: status.DND,
-//   },
-//   {
-//     id: 4,
-//     name: "Krupal",
-//     last_message: {
-//       message:
-//         "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
-//       status: message_status.SENT,
-//       date: "09/08/24",
-//     },
-//     status: status.DND,
-//   },
-//   {
-//     id: 5,
-//     name: "Dev",
-//     last_message: {
-//       message:
-//         "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
-//       status: message_status.FAILED,
-//       date: "09/08/24",
-//     },
-//     status: status.DND,
-//   },
-//   {
-//     id: 6,
-//     name: "Dharmesh",
-//     last_message: {
-//       message:
-//         "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
-//       count: 8,
-//       date: "09/08/24",
-//     },
-//     status: status.DND,
-//   },
-//   {
-//     id: 7,
-//     name: "Nandini Malviya",
-//     last_message: {
-//       message:
-//         "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
-//       count: 8,
-//       date: "09/08/24",
-//     },
-//     status: status.ONLINE,
-//   },
-//   {
-//     id: 8,
-//     name: "Anmol",
-//     last_message: {
-//       message:
-//         "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
-//       count: 8,
-//       date: "09/08/24",
-//     },
-//     status: status.OFFLINE,
-//   },
-// ];
-
 const MemberSkeleton = () => {
   return (
     <div className="px-9 py-4 flex cursor-pointer border-l">
@@ -146,14 +56,14 @@ const MemberSkeleton = () => {
   );
 };
 
-const Member = ({ data }) => {
+const Member = ({ connection }) => {
   const active = useTabState((state) => state.dTSelection);
   const setActive = useTabState((state) => state.changeDTSelection);
   return (
     <div
-      onClick={() => setActive(data)}
+      onClick={() => setActive(connection.user)}
       className={`px-10 py-4 flex cursor-pointer border-l hover:bg-blue-50 ${
-        active?._id === data._id
+        active?._id === connection.user._id
           ? "bg-blue-50 border-l-blue-500"
           : "border-l-transparent"
       }`}
@@ -161,43 +71,36 @@ const Member = ({ data }) => {
       <div className="w-12 h-12 rounded-2xl bg-slate-100 flex-shrink-0 flex justify-center items-center text-xl relative">
         <div
           className={`absolute w-4 h-4 top-[-2px] right-[-2px] rounded-full border-2 border-white ${
-            status_color[data?.status || status.OFFLINE]
+            status_color[status.OFFLINE]
           }`}
         ></div>
-        {data.username[0].toUpperCase()}
+        {connection.user.username[0].toUpperCase()}
       </div>
 
       <div className="ml-4 flex-grow min-w-0">
         <h4 className="font-semibold w-full text-ellipsis overflow-hidden whitespace-nowrap">
-          {data.full_name}
+          {connection.user.full_name}
         </h4>
         <p className="w-full text-ellipsis overflow-hidden whitespace-nowrap">
-          Loream Message
+          {connection.last_message.content}
         </p>
       </div>
 
       <div className="text-xs flex flex-col justify-center ml-4">
-        <p>Today</p>
-        {data?.last_message?.count ? (
-          <div
-            title={data?.last_message?.count}
-            className="w-4 h-4 mt-1 border bg-violet-400 rounded-full"
-          ></div>
-        ) : (
-          <p
-            className={`${
-              message_status_map[
-                data?.last_message?.status || message_status.SEEN
-              ].color
-            } font-bold`}
-          >
-            {
-              message_status_map[
-                data?.last_message?.status || message_status.SEEN
-              ].text
-            }
-          </p>
-        )}
+        <p>{DTMformatDate(connection.last_message.updatedAt)}</p>
+        <p
+          className={`${
+            message_status_map[
+              connection.last_message?.status || message_status.SEEN
+            ].color
+          } font-bold`}
+        >
+          {
+            message_status_map[
+              connection.last_message?.status || message_status.SEEN
+            ].text
+          }
+        </p>
       </div>
     </div>
   );
@@ -279,7 +182,7 @@ const List = () => {
                 if (!connection) return null;
                 return (
                   <div key={key} style={style}>
-                    <Member data={connection.user} />
+                    <Member connection={connection} />
                   </div>
                 );
               }}
---
+++ src/utils/dateFormaters.js
@@ -0,0 +1,16 @@
+import { format, isToday, isYesterday, isThisWeek, isThisYear } from "date-fns";
+
+export function DTMformatDate(timestamp) {
+  const date = new Date(timestamp);
+  if (isToday(date)) {
+    return "Today";
+  } else if (isYesterday(date)) {
+    return "Yesterday";
+  } else if (isThisWeek(date)) {
+    return format(date, "EEEE"); // Returns day of the week, e.g., 'Monday'
+  } else if (isThisYear(date)) {
+    return format(date, "dd/MM"); // Returns date in format DD/MM
+  } else {
+    return format(date, "dd/MM/yyyy"); // Returns full date in format DD/MM/YYYY
+  }
+}
